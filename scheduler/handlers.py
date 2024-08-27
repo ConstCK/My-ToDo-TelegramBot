841342a81062c89a5fc25c710656732f0c81ac49
Constantin <Constantin_CK@outlook.com>
2024-08-27 21:09:39 +0300

    Refactor some code

--- scheduler/handlers.py
+++ scheduler/handlers.py
@@ -7,10 +7,9 @@ from database.crud import get_task
 # Запланированное сообщение об истечении времени на выполнение задачи
 async def task_reminder(chat_id: int, task_id: int):
     result = await get_task(task_id)
-    if result:
-        if result.status == 'Выполняется':
-            await bot.send_message(chat_id=chat_id,
-                                   text=f'Срок выполнения задачи <b>"{result.name}"</b> истекает через 10 минут.\n'
-                                        f'Не забудьте завершить ее...',
-                                   parse_mode=ParseMode.HTML
-                                   )
+    if result and result.status == 'Выполняется':
+        await bot.send_message(chat_id=chat_id,
+                               text=f'Срок выполнения задачи <b>"{result.name}"</b> истекает через 10 минут.\n'
+                                    f'Не забудьте завершить ее...',
+                               parse_mode=ParseMode.HTML
+                               )
